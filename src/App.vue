7ea30c4a9833b4086b924b144e4b61041dfa63a9
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-04-27 19:40:17 -0300

    Add minimize method and changed divs ids/classes

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div id="nav" class="noselect">
       <div id="app-name">
         TClipper
       </div>
       <div id="app-actions">
-        <div id="button" class="hover-default">
+        <div class="button hover-default" @click="minimize">
           <i class="material-icons">minimize</i>
         </div>
-        <div id="button" class="hover-default" @click="toggleMaximize">
+        <div class="button hover-default" @click="toggleMaximize">
           <i class="material-icons">crop_3_2</i>
         </div>
-        <div id="button" class="hover-red" @click="closeApp">
+        <div class="button hover-red" @click="closeApp">
           <i class="material-icons">close</i>
         </div>
       </div>
       <!-- <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> -->
     </div>
-    <router-view/>
+    <div id="app-body">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -30,6 +32,9 @@ export default {
     },
     toggleMaximize () {
       window.Bridge.toggleMaximize()
+    },
+    minimize () {
+      window.Bridge.minimize()
     }
   }
 }
@@ -38,10 +43,12 @@ export default {
 <style>
 @import "../bower_components/material-design-icons-iconfont/dist/material-design-icons.css";
 
-body {
+html,body {
   margin: 0;
   padding: 0;
   background-color: #00171F;
+  max-height: 100%;
+  height: 100%;
 }
 
 #app {
@@ -50,6 +57,17 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+#app-body {
+  flex: 1;
+  background-color: #003459;
+  margin: 0px 10px 10px 10px;
+  padding: 5px;
+  border-radius: 5px;
 }
 
 #nav {
@@ -64,17 +82,12 @@ body {
   padding: 5px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#app-actions {
+#nav #app-actions {
   display: flex;
   -webkit-app-region: no-drag;
 }
 
-#app-actions #button {
+#app-actions .button {
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -91,7 +104,13 @@ body {
   background-color: rgb(13, 58, 73);
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.noselect {
+  -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Opera and Firefox */
 }
 </style>
