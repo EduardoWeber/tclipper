69c6abb25200909fe4f109dc1e22484a11fd6d98
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-04-27 22:56:38 -0300

    Add action buttons and description on VideoListItem

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -2,15 +2,22 @@
   <div id="video-list-item">
     <div id="video-thumbnail">
       Video
+      <div id="duration">
+        3:19
+      </div>
     </div>
     <div id="description">
       Title <br>
       Titulo Live <br>
       Streamer <br>
-      Duration
+      Categoria <br>
+      Clipado por <br>
     </div>
     <div id="actions">
-      Actions
+      <div id="buttons">
+        <i class="material-icons action-button">get_app</i>
+        <i class="material-icons action-button">cancel</i>
+      </div>
     </div>
   </div>
 </template>
@@ -24,25 +31,52 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 #video-list-item {
-  min-height: 30vh;
+  min-height: 100px;
   background-color: #007EA7;
   border-radius: 3px;
   color: white;
   display: flex;
   flex-direction: row;
+  padding: 3px;
 }
 
 #video-thumbnail {
-  flex: 1;
-  background-color: black;
-  margin: 3px;
+  width: 175px;
+  background-color: rgb(75, 75, 75);
   border-radius: 3px;
+  position: relative;
+}
+
+#duration {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin-right: 2px;
+  margin-bottom: 2px;
+  background-color: black;
+  padding: 2px;
 }
 
 #description {
-  flex: 2
+  flex: 1;
+  margin-left: 10px;
+  text-align: left;
 }
 
 #actions {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+#buttons {
+  display: flex;
+  justify-content: space-evenly;
+  min-width: 100px;
+}
+
+.action-button {
+  font-size: 28pt;
+  cursor:pointer;
 }
 </style>
