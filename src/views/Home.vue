5e2561d6594dd558ac228919f1d365deae5544da
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-04-27 21:31:53 -0300

    Add components to Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Input/>
+    <VideoListItem/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Input from '@/components/Input.vue'
+import VideoListItem from '@/components/VideoListItem.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Input,
+    VideoListItem
   }
 }
 </script>
+
+<style scoped>
+
+.home {
+  flex: 1;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+}
+</style>
