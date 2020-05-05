df87772e17ae10defae61ff2e01fc358d1110624
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-04 22:50:37 -0300

    Mock visuals

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="home">
-    <Input/>
-    <VideoListItem/>
+    <div class="header">
+      <Button buttonText="Colar link"/>
+      <Input/>
+      <Button buttonText="Cancelar todos"/>
+      <Button buttonText="Baixar todos"/>
+    </div>
+    <div class="list">
+      <VideoListItem/>
+      <VideoListItemLoading/>
+      <VideoListItem/>
+      <VideoListItem/>
+      <VideoListItem/>
+      <VideoListItem/>
+    </div>
   </div>
 </template>
 
@@ -9,12 +21,16 @@
 // @ is an alias to /src
 import Input from '@/components/Input.vue'
 import VideoListItem from '@/components/VideoListItem.vue'
+import VideoListItemLoading from '@/components/VideoListItemLoading.vue'
+import Button from '@/components/Button.vue'
 
 export default {
   name: 'Home',
   components: {
     Input,
-    VideoListItem
+    VideoListItem,
+    VideoListItemLoading,
+    Button
   }
 }
 </script>
@@ -24,7 +40,17 @@ export default {
 .home {
   flex: 1;
   display: flex;
-  height: 100%;
   flex-direction: column;
+  height: 100%;
+}
+
+.list {
+  flex: 1;
+  overflow-y: auto;
+}
+
+.header {
+  justify-content: space-between;
+  display: flex;
 }
 </style>
