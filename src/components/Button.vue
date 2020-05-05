1559d46e7e8d1af3c8117172c391c82cfac12d11
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-04 22:49:32 -0300

    Simple button

---
+++ src/components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="button">
+    <div id="button-text">
+      {{buttonText}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    buttonText: String,
+    callbackFunction: Function
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#button {
+  display: flex;
+  border-width: 0;
+  border-radius: 3px;
+  height: 30px;
+  background-color: #00A8E8;
+  align-content: center;
+  justify-content: center;
+  display: flex;
+  padding: 5px 10px;
+  margin: 5px;
+}
+
+#button-text {
+  color: white;
+  align-self: center;
+}
+</style>
