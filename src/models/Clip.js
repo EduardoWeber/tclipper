b4ede47267b1cf9e3dce820c54170b21dfb8ecc1
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-14 23:07:27 -0300

    Finish Clip model

--- src/models/Clip.js
+++ src/models/Clip.js
@@ -1,25 +1,32 @@
-const ClipStatus = {
-  NOT_STARTED: 0,
-  STARTED: 1,
-  FINISHED: 2,
-  CANCELLED: 3,
-  ERROR: 4
+export const ClipStatus = {
+  NOT_LOADED: 0,
+  LOADED: 1,
+  STARTED: 2,
+  FINISHED: 3,
+  CANCELLED: 4,
+  ERROR: 5
 }
 
-class Clip {
-  constructor(url, thumbUrl, title, streamTitle, streamer, category, clippedBy, progress = 0, data = null) {
-    if (data != null) {
-
-    } else {
-      this.url = url
-      this.progress = progress
-      this.thumbUrl = thumbUrl
-      this.title = title
-      this.streamTitle = streamTitle
-      this.streamer = streamer
-      this.category = category
-      this.clippedBy = clippedBy
+export class Clip {
+  constructor({data = null, id = null}) {
+    if (data !== null) {
+      this.id = data.id
+      this.url = data.url
+      this.embedUrl = data.embed_url
+      this.broadcasterId = data.broadcaster_id
+      this.broadcasterName = data.broadcaster_name
+      this.creatorId = data.creator_id
+      this.creatorName = data.creator_name
+      this.videoId = data.video_id
+      this.gameId = data.game_id
+      this.language = data.language
+      this.title = data.title
+      this.viewCount = data.view_count
+      this.createdAt = data.created_at
+      this.thumbnailUrl = data.thumbnail_url
       this.status = ClipStatus.NOT_STARTED
+    } else {
+
     }
   }
 }
