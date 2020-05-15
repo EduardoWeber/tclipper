df0cc039a854b6bd24aa26eb63f30793fd7bc99c
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-14 23:07:58 -0300

    Add getClip to twitch services

--- src/services/twitchServices.js
+++ src/services/twitchServices.js
@@ -1,22 +1,44 @@
 const axios = require('axios')
 
 export const getToken = async (clientId, clientSecret, grantType = 'client_credentials', scope = '') => {
-    try {
-      const url = 'https://id.twitch.tv/oauth2/token'
-      const config = {
-        method: 'post',
-        url: url,
-        params: {
-          'client_id': clientId,
-          'client_secret': clientSecret,
-          'grant_type': grantType,
-          'scope': scope
-        }
+  try {
+    const url = 'https://id.twitch.tv/oauth2/token'
+    const config = {
+      method: 'post',
+      url: url,
+      params: {
+        'client_id': clientId,
+        'client_secret': clientSecret,
+        'grant_type': grantType,
+        'scope': scope
       }
-      let response = await axios(config)
-      return response.data
-    } catch (err) {
-      throw err
     }
+    let response = await axios(config)
+    return response.data
+  } catch (err) {
+    throw err
   }
-  
+}
+
+
+export const getClip = async (token, clientId, id) => {
+  try {
+    const url = 'https://api.twitch.tv/helix/clips'
+    const config = {
+      method: 'get',
+      url: url,
+      headers: {
+        'Client-ID': clientId,
+        'Authorization': 'Bearer ' + token
+      },
+      params: {
+        'id': id
+      }
+    }
+    let response = await axios(config)
+    console.log(response)
+    return response.data
+  } catch (err) {
+    throw err
+  }
+}
