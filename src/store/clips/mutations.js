24a4643f1926e5225b1e9e274169ffded4da0cc2
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-25 21:41:32 -0300

    Add REMOVE_CLIP_FROM_LIST and UPDATE_CLIP

--- src/store/clips/mutations.js
+++ src/store/clips/mutations.js
@@ -1,6 +1,19 @@
+import Vue from 'vue'
 
 export const mutations = {
     ADD_TO_CLIP_LIST (state, clip) {
-        state.clipList.push(clip)
+        state.clipList.unshift(clip)
+    },
+    REMOVE_CLIP_FROM_LIST (state, clipIndex) {
+        state.clipList.splice(clipIndex, 1)
+    },
+    UPDATE_CLIP (state, clipUpdated) {
+        for (let i = 0; i < state.clipList.length; i++) {
+            const clip = state.clipList[i];
+            if (clip.uniqueId === clipUpdated.uniqueId) {
+                Vue.set(state.clipList, i, clipUpdated)
+                break
+            }
+        }
     }
 }
