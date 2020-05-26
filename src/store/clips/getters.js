29ab6bfb9900f1b266cd8bae393f38d6c7a8daca
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-05-25 21:41:06 -0300

    Add getClipByUniqueId to getters

--- src/store/clips/getters.js
+++ src/store/clips/getters.js
@@ -2,5 +2,14 @@
 export const getters = {
     getClipList: (state) => {
         return state.clipList
+    },
+    getClipByUniqueId: (state) => (uniqueId) => {
+        for (let i = 0; i < state.clipList.length; i++) {
+            const clip = state.clipList[i];
+            if (clip.uniqueId === uniqueId) {
+                return clip
+            }
+        }
+        return false
     }
 }
