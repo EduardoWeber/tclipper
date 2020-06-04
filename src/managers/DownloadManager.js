1a68c395162df211ace10c3dee48bda048a224cf
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-06-03 23:09:44 -0300

    Create DownloadManager

---
+++ src/managers/DownloadManager.js
@@ -0,0 +1,55 @@
+const { download } = require('electron-dl');
+import { BrowserWindow, ipcMain, app } from 'electron'
+
+export default class DownloadManager {
+  constructor(win) {
+    this.win = win
+    this.downloadItemList = []
+    this.directory = app.getPath('downloads')
+    
+    ipcMain.on("download_manager", (event, payload) => {
+      if (payload.type) {
+
+        if (payload.type === "start_download") {
+          const clipUrl = this.generateClipUrl(payload.clip.thumbnailUrl)
+          this.startDownload(clipUrl, payload.clip.uniqueId, this.directory)
+        }
+        
+      }
+    });
+  }
+
+  insertDownloadItem(uniqueId, downloadItem) {
+    this.downloadItemList.push({
+      uniqueId,
+      downloadItem
+    })
+  }
+
+  removeDownloadItem(uniqueId) {
+    for (let i = 0; i < this.downloadItemList.length; i++) {
+      const downloadItem = this.downloadItemList[i];
+      if (downloadItem.uniqueId === uniqueId) {
+        this.downloadItemList.splice(i, 1);
+        return
+      }
+    }
+    throw new Error('Could not find the specified uniqueId')
+  }
+
+  startDownload(twitchClipUrl, uniqueId, directory) {
+    const options = {
+      directory,
+      onStarted: (downloadItem) => this.insertDownloadItem(uniqueId, downloadItem)
+    }
+    download(BrowserWindow.getFocusedWindow(), twitchClipUrl, options).then((dl) => {
+      console.log("Downloaded")
+      this.removeDownloadItem(uniqueId)
+    })
+  }
+
+  generateClipUrl(clipThumbUrl) {
+    const startIndex = clipThumbUrl.indexOf('-p');
+    return clipThumbUrl.slice(0, startIndex) + '.mp4'
+  }
+}
