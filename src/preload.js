9d036b5c769349eba24fa6180a8991e1731b2cf7
EduardoWeber <eduardo-felipe-weber@hotmail.com>
2020-06-03 21:36:05 -0300

    Change api to use contextBridge

--- src/preload.js
+++ src/preload.js
@@ -1,74 +1,32 @@
 // in preload scripts, we have access to node.js and electron APIs
 // the remote web app will not have access, so this is safe
-const { ipcRenderer, remote, ipcMain } = require('electron')
+const { ipcRenderer, contextBridge } = require('electron')
 
-process.once('loaded', () => {
-  // Custom bridge, hopefully safe
-  window.bridge = {
-    // returns the new callback
-    on: (eventName, callback) => { 
-      const newCallback = (_, data) => callback(data);
-      ipcRenderer.on(eventName, newCallback);
-      return newCallback;
+contextBridge.exposeInMainWorld(
+  "api", {
+    send: (channel, data) => {
+        // whitelist channels
+        let validChannels = ["toMain", "window_manager"];
+        if (validChannels.includes(channel)) {
+            ipcRenderer.send(channel, data);
+        }
     },
-    once: (eventName, callback) => { 
-      const newCallback = (_, data) => callback(data);
-      ipcRenderer.once(eventName, newCallback);
-      return newCallback;
+    receive: {
+      // returns the new callback
+      on: (eventName, callback) => { 
+        const newCallback = (_, data) => callback(data);
+        ipcRenderer.on(eventName, newCallback);
+        return newCallback;
+      },
+      once: (eventName, callback) => { 
+        const newCallback = (_, data) => callback(data);
+        ipcRenderer.once(eventName, newCallback);
+        return newCallback;
+      },
     },
-    removeListener: (eventName, callback) => ipcRenderer.removeListener(eventName, callback),
-    removeAllListeners: (eventName) => ipcRenderer.removeAllListeners(eventName),
-  }
-  window.addEventListener('message', event => {
-    // do something with custom event
-    const message = event.data;
-
-    ipcRenderer.send('logging', message);
-
-    if (message.type) {
-
-      if (message.type === "download") {
-        ipcRenderer.send('logging', "Iniciando download" + message.properties.directory);
-        ipcRenderer.send('download', message)
-      }
-
-      if (message.type === "close_app") {
-        closeApp()
-      }
-
-      if (message.type === "toggle_maximize") {
-        toggleMaximize()
-      }
-
-      if (message.type === "minimize") {
-        minimize()
-      }
-      
+    remove: {
+      listener: (eventName, callback) => ipcRenderer.removeListener(eventName, callback),
+      allListeners: (eventName) => ipcRenderer.removeAllListeners(eventName),
     }
-  });
-});
-
-function sendTo(channel, payload) {
-  ipcRenderer.send('send_to', {
-    channel: channel,
-    payloadMessage: payload
-  });
-}
-
-function closeApp () {
-  remote.app.quit()
-}
-
-function toggleMaximize () {
-  let win = remote.getCurrentWindow()
-  if (win.isMaximized()) {
-    win.unmaximize()
-  } else {
-    win.maximize()
   }
-}
-
-function minimize () {
-  let win = remote.getCurrentWindow()
-  win.minimize()
-}
+);
